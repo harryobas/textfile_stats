21e27bed6fbab8682d437cb1c8a2cee768754f0c
Harry Obaseki <harry.obaseki@homeflow.co.uk>
2023-06-11 21:32:19 +0100

    character count implemented

--- ext/textfile_stats/src/lib.rs
+++ ext/textfile_stats/src/lib.rs
@@ -2,8 +2,8 @@ use magnus::{define_module, function, prelude::*, Error, exception};
 use std::io::{BufRead, BufReader};
 use std::fs::{File, self};
 
-fn word_count(file_path: String) -> Result<u32, Error> {
-    
+fn words_count(file_path: String) -> Result<u32, Error> {
+
     let file = match File::open(file_path.clone()) {
         Ok(file) => file,
         Err(e) => return Err(Error::new(exception::io_error(), e.to_string()))
@@ -17,15 +17,42 @@ fn word_count(file_path: String) -> Result<u32, Error> {
     }
 
     let reader = BufReader::new(file);
-    let mut word_count = 0u32;
+    let mut count = 0u32;
 
     for line in reader.lines() {
         if let Ok(line) = line {
-            word_count += line.split_whitespace().count() as u32;
+            count += line.split_whitespace().count() as u32;
         }
     }
 
-    Ok(word_count)
+    Ok(count)
+}
+
+fn chars_count(file_path: String) -> Result<u32, Error> {
+    let file = match File::open(file_path.clone()) {
+        Ok(file) => file,
+        Err(e) => return Err(Error::new(exception::io_error(), e.to_string()))
+
+    };
+    
+    let f_size = file_size(file_path.clone());
+
+    if f_size == 0 {
+        return Err(Error::new(exception::standard_error(), "File is empty".to_string()))
+    }
+
+    let reader = BufReader::new(file);
+    let mut count = 0u32;
+
+    for line in reader.lines() {
+        if let Ok(line) = line  {
+            line.split_whitespace().for_each(|w| count += w.chars().count() as u32);
+                    
+        }
+    }
+
+    Ok(count)
+
 }
 
 fn file_size(file_path: String) -> u64 {
@@ -35,7 +62,8 @@ fn file_size(file_path: String) -> u64 {
 #[magnus::init]
 fn init() -> Result<(), Error> {
     let module = define_module("TextfileStats")?;
-    module.define_singleton_method("word_count", function!(word_count, 1))?;
+    module.define_singleton_method("words_count", function!(words_count, 1))?;
+    module.define_singleton_method("chars_count", function!(chars_count, 1))?;
     module.define_private_method("file_size", function!(file_size, 1))?;
     Ok(())
 }
--- spec/textfile_stats_spec.rb
+++ spec/textfile_stats_spec.rb
@@ -1,26 +1,34 @@
 # frozen_string_literal: true
 
 RSpec.describe TextfileStats do
+
+  let(:file) { File.expand_path('spec/fixtures/test_file.txt') }
+  let(:empty_file) { File.expand_path('spec/fixtures/empty_test_file.txt')}
+
   it "has a version number" do
     expect(TextfileStats::VERSION).not_to be nil
   end
 
-  describe '.word_count' do
+  describe '.words_count' do
     it 'returns the number of words in a text file' do 
-      file = File.expand_path('spec/fixtures/test_file.txt')
-      expect(TextfileStats.word_count(file)).to eq 4
+      expect(TextfileStats.words_count(file)).to eq 322392
     end
   end
   context 'when file is empty' do 
     it 'raises error' do 
-      file = File.expand_path('spec/fixtures/empty_test_file.txt')
-      expect{TextfileStats.word_count(file)}.to raise_error(StandardError)
+      expect{TextfileStats.words_count(empty_file)}.to raise_error(StandardError)
     end
   end
   context 'when file is not found' do
     it 'raises error' do
       file = File.expand_path('spec/fixtires/no_text_file.txt')
-      expect{TextfileStats.word_count(file)}.to raise_error(IOError)
+      expect{TextfileStats.words_count(file)}.to raise_error(IOError)
+    end
+  end
+  describe 'chars_count' do 
+    it 'returns the number of characters in a text file' do 
+      expect(TextfileStats.chars_count(file)).to eq  1842499
+
     end
   end
 end
